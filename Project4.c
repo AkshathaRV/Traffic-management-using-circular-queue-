08c0f34b94dc29bec444d5d3faafc6c6253ad94e
AkshathaRV <108794119+AkshathaRV@users.noreply.github.com>
2025-03-12 17:23:23 +0530

    Add files via upload
    updated

---
+++ Project4.c
@@ -0,0 +1,18 @@
+#include "Project4.h"
+
+int main() {
+    CircularQueue road1, road2, road3;
+
+    // Initialize queues for each road
+    initializeQueue(&road1);
+    initializeQueue(&road2);
+    initializeQueue(&road3);
+
+    const char *inputFilename = "C:\\Users\\AKSHATHA R V\\Downloads\\project4_data.csv";
+    const char *outputFilename = "C:\\Users\\AKSHATHA R V\\Documents\\DSA_Notes\\project4_output.txt";
+
+    // Simulate traffic management using the provided dataset
+    simulateTraffic(&road1, &road2, &road3, "C:\\Users\\AKSHATHA R V\\Downloads\\project4_data.csv","C:\\Users\\AKSHATHA R V\\Documents\\DSA_Notes\\project4_output.txt");
+
+    return 0;
+}
